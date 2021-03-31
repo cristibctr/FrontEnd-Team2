3ed9d167498ab303940c8760c55f8399d0d7c4bc
CristinBlajut <39240222+CristinBlajut@users.noreply.github.com>
2021-03-31 23:45:15 +0300

    Add files via upload

--- src/HomePage.js
+++ src/HomePage.js
@@ -1,5 +1,24 @@
-import React from "react";
+import React from "react"
+import styles from "./styles/HomePage.css"
 
-const HomePage = (props) => <div>My Home Page is this</div>;
+const HomePage = () => {
+    return (
+        <div id="overview">
+
+            <div id="beneficiar">
+                <a href="linkbeneficiar.html" className="button"> Beneficiar </a>
+            </div>
+
+            <div id="binefacator">
+                <a href="linkbinefacator.html" className="button"> Binefacator </a>
+            </div>
+
+            <div id="footer">
+                Footer
+            </div>
+
+        </div>
+    )
+}
 
 export default HomePage;
