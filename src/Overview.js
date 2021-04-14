35b046b79e74d6f7fef27787d4f57d2e04b2cb4f
CristinBlajut <39240222+CristinBlajut@users.noreply.github.com>
2021-04-14 18:54:35 +0300

    Add files via upload

--- src/Overview.js
+++ src/Overview.js
@@ -1,5 +1,44 @@
 import React from "react";
+import styles from "./styles/Overview.css"
 
-const Overview = (props) => <div>My Overview Page is this</div>;
+const Overview = () => {
+    return (
+        <div id="pagina" id="epilepsy">
+
+            <div id="titlu">
+                <div class="raze">
+                    <span class="lumina">Together we are better!</span>
+                    <span class="gradient"></span>
+                    <span class="spotlight"></span>
+                </div>
+            </div>
+
+            <div id="articole">
+
+                <div id="glow">
+                    Who are we?
+                </div>
+                <div id="electric">
+                    Why are we doing this?
+                </div>
+
+                <div id="power">
+                    Why you should join us?
+                </div>
+
+            </div>
+
+            <div id="jumping">
+                <span>H</span>
+                <span>e</span>
+                <span>l</span>
+                <span>p</span>
+                <span>i </span>
+                <span>e</span>
+            </div>
+
+        </div >
+    )
+}
 
 export default Overview;
