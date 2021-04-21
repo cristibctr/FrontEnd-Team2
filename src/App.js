bc3e24ce43391f3914b2f28f8aff37f0db291a66
cristibctr <cristian.bucataru@gmail.com>
2021-04-21 14:38:06 +0300

    small fix

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Overview from "./Overview"
 import HomePage from "./HomePage"
 import Footer from "./Footer"
 import Register from "./Register"
+import Profile from "./Profile"
 
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
@@ -16,6 +17,7 @@ const App = () => {
                 <Route exact path="/" component={HomePage} />
                 <Route exact path="/login" component={Login} />
                 <Route exact path="/register" component={Register} />
+                <Route exact path="/profile" component={Profile} />
                 <Route exact path="/overview" component={Overview} />
             </Switch>
             <Footer />
