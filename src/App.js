8e391c0d2ab7366b04e8e19a0390c4f24e1720c7
cristibctr <cristian.bucataru@gmail.com>
2021-04-21 17:10:04 +0300

    Uncommented something

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import Overview from "./Overview"
 import HomePage from "./HomePage"
 import Footer from "./Footer"
 import Register from "./Register"
-//import Profile from "./Profile"
+import Profile from "./Profile"
 import ContactPage from "./ContactPage"
 
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
@@ -18,7 +18,7 @@ const App = () => {
                 <Route exact path="/" component={HomePage} />
                 <Route exact path="/login" component={Login} />
                 <Route exact path="/register" component={Register} />
-                {/* <Route exact path="/profile" component={Profile} /> */}
+                <Route exact path="/profile" component={Profile} />
                 <Route exact path="/overview" component={Overview} />
                 <Route exact path="/ContactPage" component={ContactPage} />
             </Switch>
