d7f81bcc7d8a1309af250fa06318f5cd4cf0ff26
cristi bucataru <cristian.bucataru@gmail.com>
2021-05-15 16:12:52 +0300

    Dynamic footer map

--- src/SiteMap.js
+++ src/SiteMap.js
@@ -1,24 +1,46 @@
 import React from "react";
 import { Link } from "react-router-dom";
 
-const MapItems = [
+const navItems = [
   { title: "Home", url: "/" },
-  { title: "Login", url: "/login" },
-  { title: "Register", url: "/register" },
-  { title: "Profile", url: "/profile" },
-  { title: "Contact", url: "/ContactPage" },
   { title: "Overview", url: "/overview" },
 ];
 
+const LoggedInNavItems = [
+  { title: "Profile", url: "/profile" },
+  { title: "Log Out", url: "/logout"}
+];
+
+const LoggedOutNavItems = [
+  { title: "Login", url: "/login" },
+  { title: "Register", url: "/register" },
+];
+
+function isLoggedIn() {
+  return localStorage.getItem("user") != null;
+}
+
 const SiteMap = () => {
   const generateNavList = () =>
-    MapItems.map((item, index) => (
+  navItems.map((item, index) => (
+      <div key={item.title}>
+        <Link to={item.url}> {item.title} </Link>
+      </div>
+    ));
+  const generateLoggedInNavList = () =>
+  LoggedInNavItems.map((item, index) => (
+      <div key={item.title}>
+        <Link to={item.url}> {item.title} </Link>
+      </div>
+    ));
+  const generateLoggedOutNavList = () =>
+  LoggedOutNavItems.map((item, index) => (
       <div key={item.title}>
         <Link to={item.url}> {item.title} </Link>
       </div>
     ));
 
-  return <div className="site-map"><h2>Site map</h2>{generateNavList()}</div>;
+  return <div className="site-map"><h2>Site map</h2>{generateNavList()}{isLoggedIn() ? generateLoggedInNavList() : generateLoggedOutNavList()}</div>;
 };
 
 export default SiteMap;
